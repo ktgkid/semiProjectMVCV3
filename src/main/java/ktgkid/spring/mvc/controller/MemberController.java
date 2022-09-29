337d1fe90e7a19e27f5c7ef0a448b909bdd20722
ktgkid <ktgkid@naver.com>
2022-09-29 15:35:37 +0900

    join, login 페이지 작성

--- src/main/java/ktgkid/spring/mvc/controller/MemberController.java
+++ src/main/java/ktgkid/spring/mvc/controller/MemberController.java
@@ -2,6 +2,7 @@ package ktgkid.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,9 +12,21 @@ public class MemberController {
         return "join/join";
     }
 
+    @PostMapping("/join")
+    public String joinok(){
+
+        return "redirect:/login"; /* 페이지가 바뀌게끔. */
+    }
+
     @GetMapping("/login")
     public String login(){
         return "join/login";
+    }
+
+    @PostMapping("/login")
+    public String loginok(){
+
+        return "redirect:/myinfo"; /* 페이지가 바뀌게끔. */
     }
 
     @GetMapping("/myinfo")
