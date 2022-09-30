5e7d9655c730694632c8ff68b67b1be41d2d30cc
ktgkid <ktgkid@naver.com>
2022-09-30 12:42:08 +0900

    simpleJdbcInsert 적용

--- src/main/java/ktgkid/spring/mvc/dao/BoardDAOImpl.java
+++ src/main/java/ktgkid/spring/mvc/dao/BoardDAOImpl.java
@@ -4,6 +4,9 @@ package ktgkid.spring.mvc.dao;
 import ktgkid.spring.mvc.vo.BoardVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +14,31 @@ import javax.sql.DataSource;
 
 @Service
 public class BoardDAOImpl implements BoardDAO {
+    //@Autowired bean 태그에 정의한 경우 생략가능. Spring 5.0 이상가능
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    
+    private SimpleJdbcInsert simpleInsert;
+
+    public BoardDAOImpl(DataSource dataSource) {
+        // 고급화 단순 기술. (insert 만 가능)
+        simpleInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("board")
+                .usingColumns("title", "userid", "content");
+        // sql 구문 생략가능.
+    }
 
     @Override
     public int insertBoard(BoardVO bvo) {
-        String sql = "insert into board (title, userid, content) values (?, ?, ?)";
+        SqlParameterSource params = new BeanPropertySqlParameterSource(bvo);
+
+        return simpleInsert.execute(params);
+
+        /*String sql = "insert into board (title, userid, content) values (?, ?, ?)";
         Object[] params = new Object[] {
                 bvo.getTitle(), bvo.getUserid(), bvo.getContent()
         };
 
-        return jdbcTemplate.update(sql, params);
+        return jdbcTemplate.update(sql, params);*/
     }
 }
--- src/main/java/ktgkid/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/ktgkid/spring/mvc/dao/MemberDAOImpl.java
@@ -4,25 +4,41 @@ import ktgkid.spring.mvc.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
+    //@Autowired bean 태그에 정의한 경우 생략가능. Spring 5.0 이상가능
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleInsert;
 
-
+    public MemberDAOImpl(DataSource dataSource) {
+        // 고급화 단순 기술. (insert 만 가능)
+        simpleInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("member")
+                .usingColumns("userid", "passwd", "name", "email");
+        // sql 구문 생략가능.
+    }
 
     @Override
     public int insertMember(MemberVO mvo) {
-        String sql = "insert into member (userid, passwd, name, email) values (?, ?, ?, ?)"; /* 암기사항. */
+        SqlParameterSource params = new BeanPropertySqlParameterSource(mvo);
+
+        return simpleInsert.execute(params);
+
+        /*String sql = "insert into member (userid, passwd, name, email) values (?, ?, ?, ?)"; *//* 암기사항. *//*
 
         Object[] params = new Object[] {
           mvo.getUserid(), mvo.getPasswd(), mvo.getName(), mvo.getEmail()
         };
 
-        return jdbcTemplate.update(sql, params);
+        return jdbcTemplate.update(sql, params);*/
     }
 }
